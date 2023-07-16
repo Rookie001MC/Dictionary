0603d4766bb650414fd5ecb1e6b66c3cf17ecbf3
idirius <122882721+idirius@users.noreply.github.com>
2023-07-16 19:56:42 +0700

    define correct and clean up trie, word and dictionary objects

---
+++ src/word.cpp
@@ -0,0 +1,45 @@
+#include "../header/word.h"
+
+// define Word
+Word::Word(std::string key, std::string type = "", std::string definition) {
+    this->key = key;
+    this->type = type;
+    this->definition = definition;
+}
+
+void Word::setKey(std::string key) {
+    this->key = key;
+}
+
+void Word::setType(std::string type) {
+    this->type = type;
+}
+
+void Word::setDefinition(std::string definition) {
+    this->definition = definition;
+}
+
+// define Dictionary
+Dictionary::Dictionary(std::string path, int dictType) {
+    fin.open(path);
+    this->dictType = dictType;
+}
+
+Dictionary::~Dictionary() {
+    fin.close();
+}
+
+Word Dictionary::getWord() {
+    switch(dictType) {
+        case 0:
+            return getWordEngEng();
+        case 1:
+            return getWordEngVie();
+        case 2:
+            return getWordVieEng();
+        case 3:
+            return getWordEmoji();
+        case 4:
+            return getWordSlang();
+    }
+}
