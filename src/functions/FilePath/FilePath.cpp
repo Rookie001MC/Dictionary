8d95f5a0f492709c92f8fdaab4f1506cbc579a99
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2023-07-26 20:56:05 +0700

    Removed unnecessary files

--- src/functions/FilePath/FilePath.cpp
+++
@@ -1,7 +0,0 @@
-#include "FilePath.h"
-
-// This should have the argv[0] passed here.
-std::filesystem::path getExecutableLocation(std::string current_dir)
-{
-    return (std::filesystem::path(current_dir).parent_path());
-}
