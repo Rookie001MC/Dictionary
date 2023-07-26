b7d5f275a763ae6087f56c95174ffa05d585a440
Rookie Nguyễn <nguyenquocthang2004@gmail.com>
2023-07-26 12:56:26 +0700

    Moved the getWord functions to word.cpp

--- src/functions/GetWordEmoji/GetWordEmoji.h
+++
@@ -1,8 +0,0 @@
-#ifndef GETWORDEMOJI_H
-#define GETWORDEMOJI_H
-
-#include "dictionary/word.h"
-#include "nlohmann/json.hpp"
-#include <fstream>
-
-#endif
--- src/functions/GetWordKaomoji/GetWordKaomoji.h
+++
@@ -1,9 +0,0 @@
-#ifndef GETWORDKAOMOJI_H
-#define GETWORDKAOMOJI_H
-
-
-#include "dictionary/word.h"
-#include "nlohmann/json.hpp"
-#include "fstream"
-
-#endif
