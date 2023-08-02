6a125185e9212b99bd5ce1d5de1a5be504d58b6f
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2023-08-02 20:22:37 +0700

    new stuffs

--- include/dictionary/frontend/paletteColors/source.h
+++
@@ -1,7 +0,0 @@
-#ifndef PALETTE_SOURCE_H
-#define PALETTE_SOURCE_H
-
-#include "raylib-cpp.hpp"
-const Color source = raylib::Color(255, 155, 155, 255);
-
-#endif
---
+++ include/dictionary/frontend/style.h
@@ -0,0 +1,23 @@
+#ifndef PALETTE_H
+#define PALETTE_H
+
+#include "raylib.h"
+#include "raygui.h"
+
+#define PRIMARY_COLOR (0x9c4144)
+#define SECONDARY_COLOR (0x845400)
+#define OUTLINE_COLOR (0x857372)
+
+#define TEXT_COLOR (0x082100)
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+#define DISPLAY_FONT_SIZE 40
+#define WORD_FONT_SIZE 30
+#define TEXT_FONT_SIZE 14
+
+#define MAX_INPUT_CHAR 256
+
+#define ITEM_HEIGHT 30
+#endif
