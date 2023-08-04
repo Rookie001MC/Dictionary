4fef31dda41a46f601e6293769e102b385361a25
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2023-08-04 18:03:42 +0700

    Chú thích bảng màu

--- include/frontend/styles.h
+++ include/frontend/styles.h
@@ -1,39 +1,71 @@
 #ifndef PALETTE_H
 #define PALETTE_H
 
+/**
+*   Bảng màu đươc lấy từ Material Design Theme Builder, sử dụng 4 màu cơ bản ở đây: 
+*   https://colorhunt.co/palette/ff9b9bffd6a5fffec4cbffa9
+* 
+*   Material Theme Builder:
+*   https://m3.material.io/theme-builder#/custom
+*
+*   Hình của bảng màu có thể xem tại folder visualisations/palette-sample.jpeg
+*   Hình của các nút dựa trên bảng màu này có thể xem tại visualisations/Components.png
+*/
+
+// Có một số hàm tạo hình của Raylib sử dụng Color, nên cần phải chuyển đổi từ hex sang RGB
+// Chính xác ra có thể sử dụng GetColor() để lấy màu từ hex, nhưng mà phê quá lỡ làm cmnr 
+// 
+// Note là nếu cần đổi màu, mã Hex cần phải đủ 8 kí tự, tức là có cả mã alpha.
+// Có thể để alpha là `FF` cho xong.
+
+// Màu chính
 #define PRIMARY_COLOR (0x9C4144ff)
 #define PRIMARY_COLOR_RGB (Color){0x9c, 0x41, 0x44, 0xff}
 
+// Màu của nút, các ô chứa, ...
 #define PRIMARY_COLOR_CONTAINER (0xFFDAD9FF)
 #define PRIMARY_COLOR_CONTAINER_RGB (Color){0xff, 0xda, 0xd9, 0xff}
 
+// Same thing nhưng khi di chuột lên
 #define PRIMARY_COLOR_CONTAINER_HOVER (0xdb7173ff)
 #define PRIMARY_COLOR_CONTAINER_HOVER_RGB (Color){0xdb, 0x71, 0x73, 0xff}
 
+// Màu phụ
 #define SECONDARY_COLOR (0x845400FF)
 #define SECONDARY_COLOR_RGB (Color){0x84, 0x54, 0x00, 0xff}
 
+// Màu của nút, các ô chứa, ... nhưng sử dụng tông màu phụ
 #define SECONDARY_COLOR_CONTAINER (0xffddb6ff)
 #define SECONDARY_COLOR_CONTAINER_RGB (Color){0xff, 0xdd, 0xb6, 0xff}
 
+// Same thing nhưng khi di chuột lên
 #define OUTLINE_COLOR (0x857372ff)
 #define OUTLINE_COLOR_RGB (Color){0x85, 0x73, 0x72, 0xff}
 
+// Màu nền
 #define BG_COLOR (0xF7FEECff)
 #define BG_COLOR_RGB (Color){0xf7, 0xfe, 0xec, 0xff}
 
+// Màu của chữ
 #define TEXT_COLOR (0x082100ff)
 #define TEXT_COLOR_RGB (Color){0x08, 0x21, 0x00, 0xff}
 
+// Window resolution
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
-#define DISPLAY_FONT_SIZE 25
+// Cỡ font chữ
+
+// Font size của từ
 #define WORD_FONT_SIZE 30
+
+// Font nội dụng bình thường
 #define TEXT_FONT_SIZE 14
+
+// 2 cái này chắc dư thừa nhma ai biết có xài hay ko :)))
+#define ITEM_HEIGHT 30
 
 #define MAX_INPUT_CHAR 256
 
-#define ITEM_HEIGHT 30
 
 #endif
