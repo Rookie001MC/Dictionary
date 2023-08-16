793ddaf418d37f501cb930a1f859a49e1a5d4ab7
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2023-08-16 14:12:29 +0700

    Moving `prebuildDictionaries()` to avoid cluttering

---
+++ include/dictionary/prebuildTries.h
@@ -0,0 +1,12 @@
+#ifndef BACKEND_PREBUILD_TRIES_H_
+#define BACKEND_PREBUILD_TRIES_H_
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include "globalVars/globalVars.h"
+#include "dictionary/build.h"
+
+void prebuildDictionaries();
+void clearPrebuiltTries();
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -11,61 +11,7 @@
 #include "dictionary/filePathHandler.h"
 #include "globalVars/globalVars.h"
 #include "dictionary/word.h"
-
-void prebuildDictionaries()
-{
-    std::cout << "\033[1;32mPrebuilding dictionaries...\033[0m\n";
-
-    std::string dataDir = Resources::currentExeDir + "../data/";
-    std::string dictDirs[] = {
-        dataDir + "engeng.dict",
-        dataDir + "engvie.dict",
-        dataDir + "vieeng.dict",
-        dataDir + "slang.dict",
-        dataDir + "emoji.dict",
-    };
-
-    Dictionary engEng = Dictionary(dictDirs[0], 0);
-    Dictionary engVie = Dictionary(dictDirs[1], 1);
-    Dictionary vieEng = Dictionary(dictDirs[2], 2);
-    // Dictionary emoji = Dictionary(dictDirs[3], 3);
-     Dictionary slang = Dictionary(dictDirs[4], 3);
-
-    auto start = std::chrono::high_resolution_clock::now();
-    build(engEng, PrebuiltTriesList[0]);
-    auto end = std::chrono::high_resolution_clock::now();
-    std::cout << "engEng: \033[;32m" << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms\033[0m\n";
-
-/*     start = std::chrono::high_resolution_clock::now();
-    build(engVie, *PrebuiltTries::engVie);
-    end = std::chrono::high_resolution_clock::now();
-    std::cout << "engVie: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms\n"; */
-
-    start = std::chrono::high_resolution_clock::now();
-    build(vieEng, PrebuiltTriesList[2]);
-    end = std::chrono::high_resolution_clock::now();
-    std::cout << "vieEng: \033[;32m" << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms\033[0m\n";
-
-/*     start = std::chrono::high_resolution_clock::now();
-    build(emoji, *PrebuiltTries::emoji);
-    end = std::chrono::high_resolution_clock::now();
-    std::cout << "emoji: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms\n"; */
-
-    start = std::chrono::high_resolution_clock::now();
-    build(slang, PrebuiltTriesList[3]);
-    end = std::chrono::high_resolution_clock::now();
-    std::cout << "slang: \033[;32m" << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms\033[0m\n";
-
-}
-
-void clearPrebuiltTries()
-{
-    std::cout << "\033[1;32mClearing prebuilt tries...\033[0m\n";
-    for (auto& trie: PrebuiltTriesList)
-    {
-        trie.clear();
-    }
-}
+#include "dictionary/prebuildTries.h"
 
 int main(int argc, const char *argv[])
 {
@@ -91,7 +37,7 @@ int main(int argc, const char *argv[])
     unloadResources();
     clearPrebuiltTries();
     CloseWindow();
-    std::cout << "\033[1;32mBye!\033[0m\n";
+    std::cout << "\033[1;31mBye!\033[0m\n";
 
     return 0;
 }
